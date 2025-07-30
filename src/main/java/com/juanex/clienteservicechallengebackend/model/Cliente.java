6e17c13cef8b2326e9b4e235c63664ab046262e1
Juan Guevara M. <juanexgue@gmail.com>
2025-07-30 09:15:24 -0500

    cliente model

---
+++ src/main/java/com/juanex/clienteservicechallengebackend/model/Cliente.java
@@ -0,0 +1,25 @@
+package com.juanex.clienteservicechallengebackend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="clientes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String codigoUnico;
+    private String nombres;
+    private String apellidos;
+    private String tipoDocumento;
+    private String numeroDocumento;
+
+
+}
