35ebe65576fe541a1b2eb22bd8bc70f423869bf9
Juan Guevara M. <juanexgue@gmail.com>
2025-07-30 09:04:57 -0500

    init commit

---
+++ src/main/java/com/juanex/clienteservicechallengebackend/ClienteServiceChallengebackendApplication.java
@@ -0,0 +1,13 @@
+package com.juanex.clienteservicechallengebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteServiceChallengebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteServiceChallengebackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juanex/clienteservicechallengebackend/ClienteServiceChallengebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanex.clienteservicechallengebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteServiceChallengebackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
