35c68bcf693fb844a5b337a226d25b0081714213
Juan Guevara M. <juanexgue@gmail.com>
2025-08-03 23:37:28 -0500

    add exception open/closed

---
+++ src/main/java/com/juanex/clienteservicechallengebackend/exeption/ClienteNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.juanex.clienteservicechallengebackend.exeption;
+
+public class ClienteNoEncontradoException  extends RuntimeException{
+    public ClienteNoEncontradoException(String message) {
+        super(message);
+    }
+}
